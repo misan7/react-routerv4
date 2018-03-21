a2c37ca9eb124b5dac5ff0e935389fd2666da59c
dev <dev@MacBook-Pro-de-dev.local>
2018-03-21 16:53:31 +0100

    nav highlight

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { NavLink, Switch, Route } from "react-router-dom";
 
-// The App component references two other components: Navigation which will display the navigation menu, 
+// The App component references two other components: Navigation which will display the navigation menu,
 // and Main which will display the contents of each view.
 
 const App = () => (
@@ -17,19 +17,25 @@ const Navigation = () => (
   <nav>
     <ul>
       <li>
-        <NavLink to="/">Home</NavLink>
+        <NavLink exact activeClassName="current" to="/">
+          Home
+        </NavLink>
       </li>
       <li>
-        <NavLink to="/about">About</NavLink>
+        <NavLink exact activeClassName="current" to="/about">
+          About
+        </NavLink>
       </li>
       <li>
-        <NavLink to="/contact">Contact</NavLink>
+        <NavLink exact activeClassName="current" to="/contact">
+          Contact
+        </NavLink>
       </li>
     </ul>
   </nav>
 );
 
-// The exact prop helps to specify that we want to match the exact path that is provided in the path prop. 
+// The exact prop helps to specify that we want to match the exact path that is provided in the path prop.
 // By adding this prop, we make sure each Route is rendered only if an exact match is made.
 
 const Main = () => (
@@ -43,8 +49,11 @@ const Main = () => (
 const Home = () => (
   <div className="home">
     <h1>Welcome to my home</h1>
-    <p>The SSL driver is down, hack the open-source circuit so we can generate the RAM program!</p>
-    <img src="https://s3.amazonaws.com/uifaces/faces/twitter/darylws/128.jpg"></img>
+    <p>
+      The SSL driver is down, hack the open-source circuit so we can generate
+      the RAM program!
+    </p>
+    <img src="https://s3.amazonaws.com/uifaces/faces/twitter/darylws/128.jpg" />
   </div>
 );
 
